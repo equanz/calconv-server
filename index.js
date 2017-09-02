135df3b95cd5af813ddc519726b28b44c74a02ec
rikyuusima <equanz324@gmail.com>
2017-09-02 13:25:25 +0900

    delete nodegit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // npm require
 const fs = require('fs')
-const Git = require('nodegit')
 const async = require('async')
+const {exec} = require('child_process')
 const schedule = require('node-schedule')
 const express = require('express')
 const app = express()
@@ -11,7 +11,7 @@ const crawl = require('./calconv-crawl')
 
 // global const
 const calconv_repo = 'https://github.com/rikyuusima/calconv.git'
-const port = 3000
+const port = process.env.PORT || 3000
 const conv_name = 'nit-tsuyama'
 const year = 2017
 
@@ -19,9 +19,10 @@ async.waterfall([
   (callback) => {
     // calconvのクローン
     if(! isDirectory('calconv')){
-      Git.Clone(calconv_repo, 'calconv')
+      exec('git clone ' + calconv_repo, (err, stdout, stderr) => {
+        callback(null)
+      })
     }
-    callback(null)
   },
   (callback) => {
     crawl.crawl(conv_name, year)
